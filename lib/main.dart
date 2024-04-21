82600f2e241a4ca8d529411087b49d04d65f004c
Brijesh Padhiyar <104890493+brijeshpadhiyar@users.noreply.github.com>
2024-04-21 09:25:20 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:coffee_animatetion/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        ensureScreenSize: true,
+        useInheritedMediaQuery: true,
+        builder: (context, child) {
+          return MaterialApp(
+            title: 'Coffee',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: const HomePage(),
+          );
+        });
+  }
+}
